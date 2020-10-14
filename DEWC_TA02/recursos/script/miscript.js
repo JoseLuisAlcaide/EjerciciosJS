53de89370d2d7760cfc1640afb2ad39fa59258f2
JoseLuisAlcaide <josealcaide2000@gmail.com>
2020-10-14 14:10:32 +0200

    edad

--- DEWC_TA02/recursos/script/miscript.js
+++ DEWC_TA02/recursos/script/miscript.js
@@ -1,9 +1,9 @@
 
 /*Crea un script JavaScript que solicite el nombre a un usuario y su edad al abrir el página.
 Tiene que mostrar esa información a través de la consola de depuración*/
-let nombre = window.prompt("Introduzca su nombre ");//la palabra window. no es necesaria
-let apellido= window.prompt("Introduzca su apellido");//la palabra window. no es necesaria
-console.log(nombre,apellido);
-let saludo=(`Encantado de conocerle ${nombre} ${apellido}`);
+let nombre = window.prompt("Introduzca su Nombre ");//la palabra window. no es necesaria
+let edad= window.prompt("Introduzca su Edad");//la palabra window. no es necesaria
+console.log(nombre,edad);
+let saludo=(`Encantado de conocerle ${nombre} con una edad ${edad}`);
 console.log(saludo);
-window.alert(`Bienvenido ${nombre} ${apellido}`);
+window.alert(`Bienvenido ${nombre} de ${edad} años`);
