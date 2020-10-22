74ffb151f4eb53c2a5d276d65b5942a8dd89b427
JoseLuisAlcaide <josealcaide2000@gmail.com>
2020-10-22 14:30:56 +0200

    ejercicio7

---
+++ DEWC_TA02/recursos/script/miscript07.js
@@ -0,0 +1,25 @@
+
+
+let numero=0
+do {
+     numero=parseInt(prompt(`Introduzca un numero `))
+     if (numero=0)
+        {
+        //  numero=window.prompt(`Introduzca un numero `);
+         
+        }
+
+    }while (numero=0);
+
+
+   /* let tiempo=0
+    do {
+      tiempo = parseInt(prompt(`Introduce el número de segundos que llevas confinado`))
+      console.log(`El valor introducido es ${tiempo}`)
+      if (tiempo<=0)
+           {
+               console.log("El número introducido no es un número valido")
+               window.alert("El número introducido no es un número valido")
+   
+           }
+       } while (tiempo<=0); */
