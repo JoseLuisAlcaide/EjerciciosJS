0d09e2c208cf8e4536fc8482bc1dbe2e111c78de
JoseLuisAlcaide <josealcaide2000@gmail.com>
2020-10-29 14:50:57 +0100

    13

--- DEWC_TA02/recursos/script/miscript13.js
+++ DEWC_TA02/recursos/script/miscript13.js
@@ -1,6 +1,21 @@
-let user_Text=prompt(`Introduzca un texto`);
-user_Text.slicer([" "])
-console.log(user_Text.length)
+const numero1=[];
+const  user_Text=[a,e,i,o,u,a,e,i,o,]; 
+let elemento=(a);
+let caja1= user_Text.indexOf(elemento);
+while (caja1!= -1){
+    numero1.push(caja1);
+    caja1=user_Text.indexOf(elemento,caja1 +1)
+}
+console.log(numero1)
+console.log(caja1)
+// const  user_Text=prompt[`Introduzca un texto`];
+// user_Text.indexOf(1);
+// user_Text.indexOf(A,E,I,O,U)
+// let contador=0
+// for(contador=0 ; contador<user_Text.length ; contador++)
 
-/*for(i=0 ; i<user_Text.length ; i++){
-}*/
+// const letra_A=user_Text("A")
+//user_Text.slicer([" "])
+// console.log(user_Text)
+// console.log(user_Text.indexOf[user_Text])
+
