5b850d97e4265e6de8eb6aad18724673fb461060
JoseLuisAlcaide <josealcaide200@gmail.com>
2020-11-01 22:59:02 +0100

    16 fin

--- DEWC_TA02/recursos/script/miscript16.js
+++ DEWC_TA02/recursos/script/miscript16.js
@@ -1,12 +1,13 @@
-let num=[]
-let numero=0
-numero[i] = parseInt(prompt(`Introduzca un numero `));
-let suma=0
-console.log(numero)
-console.log(`${num}`)
-for (let i=0 ; i<num.length ; i++){
-     numero[i] = parseInt(suma+=
-    console.log(`${num}`)
+/*Escribir un script en Javascript que imprima en la consola de depuración la suma de los
+números enteros entre 1 y n, donde n es un dato solicitado al usuario. Comprobar si esa
+suma coincide con n*(n+1)/2*/
+let numUser=parseInt(prompt(`introduzca un numero `))
+let suma=0;
+for(i=0 ; i<=numUser ; i++){
+    suma+=i;
+    console.log(`esta es la suma de los elementos ${suma}`)
 }
+document.write(`<p>La suma de todos los elementos es ${suma}</p>`)
 
-// suma+=numero[i]
+let resultado2=numUser*(numUser+1)/2;
+document.write(`<p>La segunda forma es numUser*(numUser+1)/2=${resultado2}</p>`)
