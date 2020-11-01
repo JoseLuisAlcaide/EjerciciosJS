58675b38c60b41f2af9fc0c6fabea9334ce56052
JoseLuisAlcaide <josealcaide200@gmail.com>
2020-11-01 16:56:24 +0100

    16

---
+++ DEWC_TA02/recursos/script/miscript16.js
@@ -0,0 +1,12 @@
+let num=[]
+let numero=0
+numero[i] = parseInt(prompt(`Introduzca un numero `));
+let suma=0
+console.log(numero)
+console.log(`${num}`)
+for (let i=0 ; i<num.length ; i++){
+     numero[i] = parseInt(suma+=
+    console.log(`${num}`)
+}
+
+// suma+=numero[i]
