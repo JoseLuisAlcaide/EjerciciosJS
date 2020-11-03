3e8802effc2d7565f5e360b1beb3ec71ea282532
JoseLuisAlcaide <josealcaide2000@gmail.com>
2020-11-03 13:16:41 +0100

    EJER21

---
+++ DEWC_TA02/recursos/script/miscript20.js
@@ -0,0 +1,21 @@
+let numUser=0;
+// for(let i=0 ; i<=100 ; i++)
+do {
+numUser=parseInt(prompt(`Intruduzca un numero entero entre 1 y 100`))
+if ((numUser<=0) || (numUser>100)) {
+    window.alert(`el número introducido es ${numUser}; número erroneo vuelva a introducir un numero entre 1 y 100`)
+    console.log(numUser)
+    }
+}while ((numUser<=0) || (numUser>100))
+
+do {
+if (numUser%2==0) {
+    numUser=numUser/2;
+    console.log(`${numUser} `);
+    document.write(` ${numUser} ->`)
+}else {
+    numUser=numUser*3+1;
+    console.log(`${numUser} `);
+    document.write(` ${numUser} ->`)
+}
+}while (numUser !=1)
---
+++ DEWC_TA02/recursos/script/miscript21.js
@@ -0,0 +1,10 @@
+const numEuler=Math.exp(1);
+const mumE=Math.E;
+console.log(numEuler)
+document.write(numEuler)
+/*
+do{
+    numE
+
+}while (e<0.0001)
+*/
